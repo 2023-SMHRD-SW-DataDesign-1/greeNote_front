794a7104b8e1844bea4bdd51639aca3b053674b0
KwangYoungHeo <sslgo0214@gmail.com>
2023-10-16 10:59:21 +0900

    1016_10:59_허광영_리액트<->스프링 서버연결 완료

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import axios from 'axios';
 
 const Item = () => {
 
-    const testConnect = () => {
+    const testConnect = async () => {
         let obj = {
             test: '테스트통신'
         };
-
-        axios.get('http://localhost:8088/')
+        await axios.post(`http://localhost:8088/MainProject/test.do`, obj)
+        .then((res)=>{
+            console.log(res.data);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
     }
+
   return (
     <div>
         <h3>Item</h3>
-        <button>테스트 클릭</button>
+        <button onClick={testConnect}>테스트 클릭</button>
     </div>
   )
 }
