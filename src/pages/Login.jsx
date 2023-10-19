15d743b7d495c5db9c18015c01ceaa5c80b79c87
pbnc1123 <piohahen@gmail.com>
2023-10-19 10:01:55 +0900

    10:01_유한솔_로그인/회원가입 페이지 form태그 생성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,19 +14,21 @@ const Login = () => {
           <br />이용을 위해 로그인 해 주세요.</span>
       </div>
 
-      <input
-        className='join_id'
-        placeholder='아이디'
-      />
-      <input
-        className='join_pw'
-        placeholder='비밀번호'
-      />
-      <br /><br /><br /><br />
+      <form onSubmit={1}>
+        <input
+          className='join_id'
+          placeholder='아이디'
+        />
+        <input
+          className='join_pw'
+          placeholder='비밀번호'
+        />
+        <br /><br /><br /><br />
 
-      <div className='login_button'>
-        <Link to="/" className='button_link'>로그인</Link>
-      </div>
+        <button type='submit' className='login_button'>
+          <Link to="/" className='button_link'>로그인</Link>
+        </button>
+      </form>
 
     </div >
   )
