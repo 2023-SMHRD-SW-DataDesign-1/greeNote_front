9d96aeaf877af12a1cd6f944ac46d4af4f6111ac
rlawlgp0197 <rlawlgp0197@gmail.com>
2023-10-20 17:52:40 +0900

    17:51 김지혜 생성 ai 리턴페이지 웹버전

---
+++ src/components/AiReturnBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AiPlant from './AiPlant'
+import AiReturnPhoto from './AiReturnPhoto'
+
+const AiReturnBox = () => {
+  return (
+    <div className='web_box'>
+    <AiPlant/>
+    <AiReturnPhoto/>
+    </div>
+  )
+}
+
+export default AiReturnBox
