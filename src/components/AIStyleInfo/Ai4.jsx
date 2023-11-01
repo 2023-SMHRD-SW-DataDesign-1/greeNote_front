1ab79e74fe5a83952835111facc6dca8657eaed1
rlawlgp0197 <rlawlgp0197@gmail.com>
2023-11-01 12:37:48 +0900

    12:37 김지혜 ai스타일 모달창 떨림 수정

--- src/components/AIStyleInfo/Ai4.jsx
+++ src/components/AIStyleInfo/Ai4.jsx
@@ -3,10 +3,31 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const Ai4 = () => {
-  return (
-    <div>Ai4</div>
-  )
+class Ai4 extends React.Component {
+    render() {
+    const settings = {
+        infinite: true, // 무한 루프
+        speed: 150, // 슬라이드 전환 속도 (밀리초)
+        slidesToShow: 1, // 보여질 슬라이드 수
+        slidesToScroll: 1, // 슬라이드 전환 시 이동할 슬라이드 수
+        autoplay: true,
+        fade: true,
+        arrows: false
+    };
+
+    return (
+        <div className='style_top_container'>
+            <Slider {...settings}>
+                <div className='style_photo'>
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM00UituvDY0iKE4QV87j7kScB55TpgXssCQ&usqp=CAU'/>
+                </div>
+                <div className='style_photo'>
+                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdWAi5EOzJgrHJ6GvUO-vvwifPr-jX9nLrFQ&usqp=CAU'/>
+                </div>
+            </Slider>
+        </div>
+    );
+}
 }
 
 export default Ai4
