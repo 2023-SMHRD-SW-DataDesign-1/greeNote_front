b0309d981e2131bd7d1c4212d86eda79821871d1
rlawlgp0197 <rlawlgp0197@gmail.com>
2023-11-02 12:35:26 +0900

    12:35 김지혜 intro

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -7,24 +7,26 @@ import ItemIntroSlide from '../components/ItemIntroSlide'
 
 const Intro = () => {
   return (
-    <div className='bodyController'>
+    <div className='intro_top_container'>
+      <div>
+        <img className='intro_logo2' src="/Image/home_logo.png" alt="" />
+      </div>
       <div className='intro_container'>
-        <div className="intro_logo">
-        {/* 이미지 */}
-          <ItemIntroSlide/>
+        <div className='intro_slide_container'>
+          <div className="intro_slide">
+            {/* 이미지 */}
+            <ItemIntroSlide />
+          </div>
         </div>
-        <div className='buttons'>
-          <div className='login_button'>
+        <div className='intro_buttons'>
+          <div className='intro_login_button'>
             <Link to="/Login" className='button_links'>로그인</Link>
           </div>
-          <div className='join_button'>
+          <div className='intro_join_button'>
             <Link to="/Join" className='button_links'>회원가입</Link>
           </div>
         </div>
       </div>
-
-
-
     </div>
 
   )
